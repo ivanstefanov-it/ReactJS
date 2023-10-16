449a57c2377ceed4868a8afe7178f4e264f98dfa
ivan.stefanov <ivanstefanov.it@gmail.com>
2023-10-16 13:19:33 +0300

    Add first react application, and working with simple components

---
+++ Intro-To-React-And-JSX/first-react-app/src/components/Footer.js
@@ -0,0 +1,7 @@
+function Footer(){
+    return(
+        <div className="site-footer">All rights reserved &copy;</div>
+    );
+}
+
+export default Footer;
---
+++ Intro-To-React-And-JSX/first-react-app/src/components/Main.js
@@ -0,0 +1,9 @@
+function Main(){
+    return(
+        <main className="site-main">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium nemo voluptas voluptate ad voluptatum minus illo alias debitis, itaque fugiat.</p>
+        </main>
+    );
+}
+
+export default Main;
